34540c9f3ecb4f088b8a720dfe54a61f164bcd7b
dimasadh <61277501+dimasadh@users.noreply.github.com>
2024-09-03 23:23:55 +0700

    Revert "debug env"
    This reverts commit f1f1348a9ef9f7521f7485d6d39912d2384d5c13.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from services.TelegramBotService import TelegramBotService
-import os
 
 def main():
     telegram_bot_service = TelegramBotService()
@@ -15,20 +14,7 @@ def runClaudePlayground():
     claude_service = ClaudeAIService()
     claude_service.run()
 
-def checkEnv():
-    print(os.getenv("TELEGRAM_BOT_API_KEY"))
-    print(os.getenv("ANTHROPIC_API_KEY"))
-    print(os.getenv("OPEN_AI_API_KEY"))
-
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    print(os.getenv("TELEGRAM_BOT_API_KEY"))
-    print(os.getenv("ANTHROPIC_API_KEY"))
-    print(os.getenv("OPEN_AI_API_KEY"))
-
 if __name__ == '__main__':
-    checkEnv()
     main()
     # runClaudePlayground()
     # runChatGPTPlayground()
